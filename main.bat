26b4507e0c443aa548c38310728076abfaa5dcf1
jsklimavicz <jsklimavicz@gmail.com>
2021-11-11 17:27:47 -0500

    Updated .bat file to add .dll to path for Windows

--- main.bat
+++ main.bat
@@ -1,3 +1,3 @@
-set PYTHON_HOME=C:\Users\BiotekDT.TYRATECH\Anaconda3\;C:\Users\BiotekDT.TYRATECH\Anaconda3\Scripts\;C:\Users\BiotekDT.TYRATECH\Anaconda3\Library\bin
+set PYTHON_HOME=C:\msys64\mingw64\bin;C:\Users\BiotekDT.TYRATECH\Anaconda3\;C:\Users\BiotekDT.TYRATECH\Anaconda3\Scripts\;C:\Users\BiotekDT.TYRATECH\Anaconda3\Library\bin
 PATH %PYTHON_HOME%;%PATH
 python ./main.py
